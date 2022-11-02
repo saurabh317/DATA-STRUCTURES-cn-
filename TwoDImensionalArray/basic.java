a7ff7492f259b5feebbca88088406d9a3b5a1be1
saurabh317 <saurabhsp317@gmail.com>
2022-11-02 23:48:31 +0530

    added 2d array

---
+++ TwoDImensionalArray/basic.java
@@ -0,0 +1,10 @@
+package TwoDImensionalArray;
+
+public class basic {
+    public static void main(String[] args) {
+        int arr[][]=new int[2][3];
+        arr[1][2]=1;
+        System.out.println(arr[1][2]);
+
+    }
+}
