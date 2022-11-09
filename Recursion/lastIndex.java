93b2104efea006b0d7f407ff77e7ed4d91664327
saurabh317 <saurabhsp317@gmail.com>
2022-11-09 19:35:57 +0530

    added prgm

---
+++ Recursion/lastIndex.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class lastIndex {
+    public static int checkForLastIndex(int[] arr,int StartIndex,int x){
+        if(StartIndex == arr.length){
+            return -1;
+        }
+        int store = checkForLastIndex(arr,StartIndex+1,x);
+        if(store != -1){
+            return store;
+        }
+        else if(arr[StartIndex] == x ){
+            return StartIndex;
+        }
+        else{
+            return -1;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr ={3,2,3,4,5,5};
+        System.out.println(checkForLastIndex(arr,0,3));
+
+    }
+}
