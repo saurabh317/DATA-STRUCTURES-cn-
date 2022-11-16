883f47558b567a962a981983648edb8190a50778
saurabh317 <saurabhsp317@gmail.com>
2022-11-16 23:37:08 +0530

    started oops

---
+++ OPPS1/demoClass.java
@@ -0,0 +1,13 @@
+package OPPS1;
+
+public class demoClass {
+    String name;
+    int roll_n0;
+    public static void main(String[] args) {
+        demoClass obj = new demoClass();
+        obj.name = "saurabh";
+        obj.roll_n0 = 732;
+        System.out.println("my name is " +obj.name+ " and my roll no is:  "+ obj.roll_n0);
+
+    }
+}
